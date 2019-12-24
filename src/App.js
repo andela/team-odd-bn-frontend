e579636a8c2de4f62faa79a1229975ae0dade9b3
Victor Karangwa <victorkarangwa4@gmail.com>
2019-12-24 11:17:45 +0200

    ch(circleCI): update circleCI - Integrate circleCI with project - Add circleCI status babdge [Start #169258569]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './Redux/store/index';
+import store from './redux/store/index';
 import Demo from './components/Demo';
 
 
