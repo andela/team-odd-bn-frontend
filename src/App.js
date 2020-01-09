8b9245ff4cde18f04092fc443e0fd795d92f8634
Ivy Nzioka <nziokaivy@gmail.com>
2020-01-09 10:09:25 +0200

    ch(styling):design dashboard home page - Add dashboard home page layout - Add styling to the page - Add most travelled destination -Add booking details - Add request details
    [Finishes #170618059]

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Signin from './containers/user/signin/signin';
 import Notfound from './NotFound';
 import SocialLogin from './containers/user/social/SocialAuthLogin';
 import Dashboard from './views/Dashboard/index';
+import HomePage from './components/HomePage';
 import './assets/css/App.scss';
 
 const App = () => (
@@ -24,6 +25,7 @@ const App = () => (
           <Route path="/signup" exact component={Signup} />
           <Route path="/reset-password" exact component={resetPasswordView} />
           <Route path="/forgot-password" exact component={forgotPasswordView} />
+          <Route path="/home" exact component={HomePage} />
           <Route component={Notfound} />
         </Switch>
       </div>
